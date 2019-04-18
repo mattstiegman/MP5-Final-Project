81f9564ecbc94956eda83cd5b85975e43e209dd2
jiayulin <jiayu6@illinois.edu>
2019-04-18 16:37:16 -0500

    Created Gameplay scene
    1. Created gameply scence, added a button that goes back to the main menu.

---
+++ Mp5 (Matt)/Assets/gamePlay.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gamePlay : MonoBehaviour
+{
+    public void backtoMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Mp5 (Matt)/Assets/mainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour {
+
+    public void gamePlay ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); 
+        //after clicking play, this will go to the gameplay.
+    }
+    public void gameQuit()
+    {
+        Debug.Log("closed app");
+        Application.Quit();
+        // quick the app
+    }
+}
