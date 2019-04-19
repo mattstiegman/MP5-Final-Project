dec8a7a7749a1c51f7f21a0583978b25d2cb566e
jiayulin <jiayu6@illinois.edu>
2019-04-19 12:23:11 -0500

    Nothing
    I didn't really do anything, just tested a bunch of things out. I have removed all my changes, so it is pretty much the same like before.

--- Mp5 (Matt)/Assets/gamePlay.cs
+++ Mp5 (Matt)/Assets/gamePlay.cs
@@ -5,6 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class gamePlay : MonoBehaviour
 {
+    private void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+            touchPosition.z = 0f;
+            transform.position = touchPosition;
+        }
+    }
     public void backtoMenu()
     {
         SceneManager.LoadScene(0);
