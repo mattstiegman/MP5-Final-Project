a6341a77e9a191c0a4bed187165e8ff32f28229c
jiayulin <jiayu6@illinois.edu>
2019-04-22 19:41:11 -0500

    Update 04/22/2019
    1. added basic background.
    2. added pixel character.

---
+++ Mp5 (Matt)/Assets/Script/gamePlay.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gamePlay : MonoBehaviour
+{
+    private void Update()
+    {
+        Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
+        /**Since we are in the update(), this means everytime the frame will be updated.
+        Therefore the Time.deltaTime is necessary for refreshing the frame. 
+        **/
+        transform.position = transform.position + horizontal * Time.deltaTime; 
+
+    }
+}
