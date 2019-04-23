e344aea8a6dc6a42c4dd0274af2a1ab09b3267c7
jiayulin <jiayu6@illinois.edu>
2019-04-22 19:55:29 -0500

    Update 04/22/2019
    1. added character movement.

--- Mp5 (Matt)/Assets/Script/gamePlay.cs
+++ Mp5 (Matt)/Assets/Script/gamePlay.cs
@@ -7,11 +7,12 @@ public class gamePlay : MonoBehaviour
 {
     private void Update()
     {
-        Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
+        Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 
+            Input.GetAxis("Vertical"), 0.0f);
         /**Since we are in the update(), this means everytime the frame will be updated.
         Therefore the Time.deltaTime is necessary for refreshing the frame. 
         **/
-        transform.position = transform.position + horizontal * Time.deltaTime; 
+        transform.position = transform.position + horizontal * Time.deltaTime;
 
     }
 }
