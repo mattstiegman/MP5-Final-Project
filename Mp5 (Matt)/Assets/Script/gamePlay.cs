794d1f83d0f48ae3d7f9178e4fa6734fc3329265
jiayulin <jiayu6@illinois.edu>
2019-04-24 12:12:24 -0500

    Update 04/24/2019
    1. added joystick pack

--- Mp5 (Matt)/Assets/Script/gamePlay.cs
+++ Mp5 (Matt)/Assets/Script/gamePlay.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class gamePlay : MonoBehaviour
-{
-    private void Update()
-    {
-        Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 
-            Input.GetAxis("Vertical"), 0.0f);
+public class gamePlay : MonoBehaviour {
+    public GameObject arrowPrefab;
+
+    private void Update() {
         /**Since we are in the update(), this means everytime the frame will be updated.
         Therefore the Time.deltaTime is necessary for refreshing the frame. 
         **/
+        Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 
+                                         Input.GetAxis("Vertical"), 0.0f);
+
         transform.position = transform.position + horizontal * Time.deltaTime * 15;
 
     }
