b8dec1653a3fa7dc9ef12466cad9337af5578f53
jiayulin <jiayu6@illinois.edu>
2019-04-26 12:50:58 -0500

    Update 04/26/2019
    1. added cross hair.
    2. added shooting joystick.
    3. arrows can now be shot in different directions.
    4. the fire rate of arrows is limited.

--- Mp5 (Matt)/Assets/Script/gamePlay.cs
+++ Mp5 (Matt)/Assets/Script/gamePlay.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class gamePlay : MonoBehaviour {
-    public GameObject arrowPrefab;
+
+    public GameObject arrowPrefab; // prefab of the arrow.
+    public GameObject crossHair; // objecet of crosshair.
+    float fireRate = 0.5f;
+    float lastShot = 0f;
+
+
     // Joystick Class/Variable that is used instead of the Input.GetAxis call.
-    public Joystick joystick;
+    public Joystick joystickMovement; // this controls character's movement.
+    public Joystick joystickAim; // this controls the character's aim.
+
+
     private void Update() {
         /**Since we are in the update(), this means everytime the frame will be updated.
         Therefore the Time.deltaTime is necessary for refreshing the frame.
@@ -14,17 +23,38 @@ public class gamePlay : MonoBehaviour {
         As mentioned above, the Joystick class is used for movement, because the
         assets have a built in script for detecting onClick and sprite movement.
         **/
-        Vector3 horizontal = new Vector3(joystick.Horizontal, 
-                                         joystick.Vertical, 0.0f);
-
+        Vector3 horizontal = new Vector3(joystickMovement.Horizontal,
+                                         joystickMovement.Vertical, 0.0f);
         transform.position = transform.position + horizontal * Time.deltaTime * 15;
 
-        // Shooting arrows
-        if (Input.GetButtonDown("Fire1")) {
-            GameObject arrow = Instantiate(arrowPrefab,
-                                          transform.position,
-                                          Quaternion.identity);
-            arrow.GetComponent<Rigidbody2D>().velocity = new Vector2(60.0f, 0);
+        // Aiming and shooting arrows.
+        aimAndShoot();
+    }
+    private void aimAndShoot() {
+        Vector3 aim = new Vector3(joystickAim.Horizontal, joystickAim.Vertical, 0.0f);
+        Vector2 shootingDirection = new Vector2(joystickAim.Horizontal, joystickAim.Vertical);
+        GameObject arrow; 
+        if (aim.magnitude > 0.0f)
+        {   //normalize the vector 1 unit long. 
+            //That being said, the crosshair can only be 1 unit away from the player.
+            aim.Normalize();
+            aim *= 5.0f; //increase the distance between the player and the crosshair.
+            crossHair.transform.localPosition = aim;
+            crossHair.SetActive(true);
+            if (aim.magnitude > 0.0f) {
+                if (Time.time > fireRate + lastShot) {
+                    arrow = Instantiate(arrowPrefab, transform.position, Quaternion.identity);
+                    arrow.GetComponent<Rigidbody2D>().velocity = shootingDirection * 30;
+                    //The arrow.transform.rotate rotates the direction of arrows in accordance to the crosshair. 
+                    arrow.transform.Rotate(0.0f, 0.0f, Mathf.Atan2(shootingDirection.y, shootingDirection.x) * Mathf.Rad2Deg);
+                    //Destory() will destory the arrows in two seconds.
+                    Destroy(arrow, 2.0f);
+                    lastShot = Time.time;
+                }
+            }
+        } else {
+            //if player didn't move the crosshair, the crosshair will disappear.
+            crossHair.SetActive(false);
         }
     }
 }
