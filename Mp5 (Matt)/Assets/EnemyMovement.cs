19f1c1646ef95a2d0601c309ec9321ae99efd271
mattstiegman <47202231+mattstiegman@users.noreply.github.com>
2019-04-27 03:51:25 -0500

    Enemy Update!
    There is a GameObject that controls enemy spawns with a script, by randomly choosing 1 of 3 available spawn locations. Another script controls the movement, which is chosen at random.
    
    Fixed a camera issue with sprites not appearing in the game tab. This was done by tweaking the Z coordinate values of some sprites and the camera position.

---
+++ Mp5 (Matt)/Assets/EnemyMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    GameObject target;
+    float moveSpeed;
+    Vector3 direction;
+    void Start()
+    {
+        //When the object is created, its target is set to the player and given a random move speed.
+        target = GameObject.Find("Player");
+        rb = GetComponent<Rigidbody2D>();
+        moveSpeed = Random.Range(4f, 8f);
+    }
+
+    
+    void Update()
+    {
+        //Enemies are moved every update.
+        MoveEnemy();
+    }
+    void MoveEnemy()
+    {
+        if (target != null)
+        {
+            //The object and Rigibody are moved in the direction of the player from the enemies current position.
+            direction = (target.transform.position - transform.position).normalized;
+            rb.velocity = new Vector2(direction.x * moveSpeed,
+                direction.y * moveSpeed);
+        }
+        //If the player dies, enemies stop moving.
+        else
+            rb.velocity = Vector3.zero;
+    }
+}
---
+++ Mp5 (Matt)/Assets/EnemySpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    //An array of spawns and enemies are made, so that more spawns and enemies can be later implemented.
+    public Transform[] spawnPoints;
+    public GameObject[] enemies;
+    int randomSpawnPoint, randomEnemy;
+    public static bool spawnAllowed;
+    void Start()
+    {
+        //Enemies are spawned at a rate declared below.
+        spawnAllowed = true;
+        InvokeRepeating("SpawnAnEnemy", 1f, 2f);
+    }
+
+    
+    void SpawnAnEnemy()
+    {
+        if (spawnAllowed)
+        {
+            //Script randomly selects which and where enemies spawn.
+            randomSpawnPoint = Random.Range(0, spawnPoints.Length);
+            randomEnemy = Random.Range(0, enemies.Length);
+            Instantiate(enemies[randomEnemy], spawnPoints[randomSpawnPoint].position,
+                Quaternion.identity);
+        }
+    }
+}
